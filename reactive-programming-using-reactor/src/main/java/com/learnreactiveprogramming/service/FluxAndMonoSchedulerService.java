806807912692ccdb668624f1e6de7786c8a0a62c
Bheemshekhar <bheemshekhar.golla@gmail.com>
2025-05-23 12:17:06 +0530

    Data Parallelism in Project Reactor

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulerService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulerService.java
@@ -2,6 +2,8 @@ package com.learnreactiveprogramming.service;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.ParallelFlux;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
@@ -14,6 +16,7 @@ public class FluxAndMonoSchedulerService {
     static List<String> namesList = List.of("alex", "ben", "chloe");
     static List<String> namesList1 = List.of("adam", "jill", "jack");
 
+    //PublishOn
     public Flux<String> explore_publishOn(){
         var namesFlux =  Flux.fromIterable(namesList)
                 .publishOn(Schedulers.parallel())
@@ -28,6 +31,7 @@ public class FluxAndMonoSchedulerService {
         return namesFlux.mergeWith(namesFlux1);
     }
 
+    //SubscribeOn
     public Flux<String> explore_subscribeOn(){
         var namesFlux =  getMap(namesList)
                 .subscribeOn(Schedulers.parallel())
@@ -46,6 +50,62 @@ public class FluxAndMonoSchedulerService {
                 .log();
 
         return namesFlux.mergeWith(namesFlux1);
+    }
+
+    //Parallel task in Reactive Streams
+    public ParallelFlux<String> explore_parallel() {
+        //To get number of cores
+        var noOfCores = Runtime.getRuntime().availableProcessors();
+        log.info("Number of Cores: {}", noOfCores);
+
+         return  Flux.fromIterable(namesList)
+                .parallel()
+                 .runOn(Schedulers.parallel())
+                .map(this::upperCase)
+                .log();
+
+    }
+
+    //Parallel task in Reactive Streams using FlatMap
+    public Flux<String> explore_parallel_usingFlatMap() {
+        return  Flux.fromIterable(namesList)
+                .flatMap(name -> {
+                    return Mono.just(name)
+                            .map(this::upperCase)
+                            .subscribeOn(Schedulers.parallel());
+                })
+                .log();
+    }
+
+    public Flux<String> explore_parallel_usingFlatMap1(){
+        var namesFlux =  Flux.fromIterable(namesList)
+                .flatMap(name -> {
+                    return Mono.just(name)
+                            .map(this::upperCase)
+                            .subscribeOn(Schedulers.parallel());
+                })
+                .log();
+
+        var namesFlux1 =  Flux.fromIterable(namesList1)
+                .flatMap(name -> {
+                    return Mono.just(name)
+                            .map(this::upperCase)
+                            .subscribeOn(Schedulers.parallel());
+                })
+                .log();
+
+        return namesFlux.mergeWith(namesFlux1);
+    }
+
+    //Parallel task in Reactive Streams using FlatMapSequential
+    public Flux<String> explore_parallel_usingFlatMapSequential() {
+        return  Flux.fromIterable(namesList)
+                .flatMapSequential(name -> {
+                    return Mono.just(name)
+                            .map(this::upperCase)
+                            .subscribeOn(Schedulers.parallel());
+                })
+                .log();
     }
 
     private Flux<String> getMap(List<String> namesList) {
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulerServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulerServiceTest.java
@@ -37,4 +37,56 @@ class FluxAndMonoSchedulerServiceTest {
                 .expectNextCount(6)
                 .verifyComplete();
     }
+
+    @Test
+    void explore_parallel() {
+        //given - precondition or setup
+
+        //when - action or behaviour which we are going to test
+        var namesFlux = fluxAndMonoSchedulerService.explore_parallel();
+
+        //then - verify output
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_parallel_usingFlatMap() {
+        //given - precondition or setup
+
+        //when - action or behaviour which we are going to test
+        var namesFlux = fluxAndMonoSchedulerService.explore_parallel_usingFlatMap();
+
+        //then - verify output
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_parallel_usingFlatMap1() {
+        //given - precondition or setup
+
+        //when - action or behaviour which we are going to test
+        var namesFlux = fluxAndMonoSchedulerService.explore_parallel_usingFlatMap1();
+
+        //then - verify output
+        StepVerifier.create(namesFlux)
+                .expectNextCount(6)
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_parallel_usingFlatMapSequential() {
+        //given - precondition or setup
+
+        //when - action or behaviour which we are going to test
+        var namesFlux = fluxAndMonoSchedulerService.explore_parallel_usingFlatMapSequential();
+
+        //then - verify output
+        StepVerifier.create(namesFlux)
+                .expectNext("ALEX", "BEN", "CHLOE")
+                .verifyComplete();
+    }
 }
